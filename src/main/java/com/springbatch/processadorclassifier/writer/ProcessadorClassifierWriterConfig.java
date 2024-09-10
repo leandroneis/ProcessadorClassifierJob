911ab362cf896b0f1fd2772edeec0ce838b0fb05
Leandro Neis <lgn200@gmail.com>
2024-09-10 20:49:01 -0300

    50. Prática: Processadores compostos com classificador

--- src/main/java/com/springbatch/processadorclassifier/writer/ProcessadorClassifierWriterConfig.java
+++ src/main/java/com/springbatch/processadorclassifier/writer/ProcessadorClassifierWriterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProcessadorClassifierWriterConfig {
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+
 	@Bean
 	public ItemWriter processadorClassifierWriter() {
 		return items -> items.forEach(System.out::println);
